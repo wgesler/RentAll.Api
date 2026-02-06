ea53bc39705cf7ecb81bab67d7c032e324515f41
Wendy Gesler <wgesler@triangleexperience.com>
2026-02-06 06:28:07 -0700

    Updated roles

--- RentAll.Domain/Enums/RoleType.cs
+++ RentAll.Domain/Enums/RoleType.cs
@@ -6,12 +6,20 @@ public enum RoleType
 	SuperAdmin = 1,
 	Admin = 2,
 	Accounting = 3,
-	Agent = 4,
-	PropertyManager = 5,
-	Facilities = 6,
-	Housekeeping = 7,
-	Corporation = 8,
-	Client = 9
+	AccountingAdmin = 4,
+	Agent = 5,
+	AgentAdmin = 6,
+	PropertyManager = 7,
+	PropertyManagerAdmin = 8,
+	Facilities = 9,
+	FacilitiesAdmin = 10,
+	Housekeeping = 11,
+	HousekeepingAdmin = 12,
+	Corporation = 13,
+	CorporationAdmin = 14,
+	Vendor = 15,
+	VendorAdmin = 16,
+	Client = 17
 }
 
 
