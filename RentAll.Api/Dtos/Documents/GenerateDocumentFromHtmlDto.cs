4bda5f6257d52ffdcd7f25f3de4bbb314bdf4c0a
Wendy Gesler <wgesler@triangleexperience.com>
2026-02-25 07:09:08 -0700

    Fix generate document

--- RentAll.Api/Dtos/Documents/GenerateDocumentFromHtmlDto.cs
+++ RentAll.Api/Dtos/Documents/GenerateDocumentFromHtmlDto.cs
@@ -1,13 +1,11 @@
-using RentAll.Domain.Enums;
-
 namespace RentAll.Api.Dtos.Documents;
 
 public class GenerateDocumentFromHtmlDto
 {
     public Guid OrganizationId { get; set; }
     public int? OfficeId { get; set; }
-    public Guid PropertyId { get; set; }
-    public Guid ReservationId { get; set; }
+    public Guid? PropertyId { get; set; }
+    public Guid? ReservationId { get; set; }
     public int DocumentTypeId { get; set; }
     public string HtmlContent { get; set; } = string.Empty;
     public string FileName { get; set; } = string.Empty;
